a9f7f999810123b69f7abef82a81bf9357563329
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-10-23 23:01:38 -0400

    Able to fight and interact with bandit monster. Basic attack functions and additionals. Need to implement logic for actions at increased levels.

--- src/monsters/humanoid_src/bandit.py
+++ src/monsters/humanoid_src/bandit.py
@@ -1,7 +1,13 @@
 '''Bandit Module for Dungeon Dudes'''
 from typing import Dict, Tuple
+from random import choice, randint
 
-class Bandit:
+from src.dd_data import LimitedDict
+from ..humanoid import Humanoid
+from ...dd_data import LimitedDict
+from ...combat_action import CombatAction
+
+class Bandit(Humanoid):
     '''Bandit Module'''
     stats_structure: Dict[str, Tuple[int]] = {"Hit Points": (75, 20),
                                               "Strength": (5, 1),
@@ -15,5 +21,56 @@ class Bandit:
         super().__init__(f"Pack of Bandits",
                          level_mod,
                          self.stats_structure)
+        self._dam_modifiers = LimitedDict(("Physical", "Fire"), default_value=100)
         
+    @property
+    def damage_modifiers(self) -> LimitedDict:
+        return self._dam_modifiers
     
+    @property
+    def defense_modifiers(self) -> LimitedDict:
+        return self._def_modifiers
+    
+    def get_skills(self) ->Dict[str, 'function']:
+        return {}
+    
+    def get_skills_list(self) -> list:
+        skills_list = ["Fireball", "Dirty Tricks"]
+        return skills_list
+
+    
+    def fireball(self) -> Tuple:
+        # damage_type = "Fire"
+        damage_amt = self.intelligence
+        msg: str = "Bandit Sorcerer casts fireball, dealing <value> fire damage"
+        return ("Attack", damage_amt, "Fire", msg)
+        # return CombatAction([("Attack",
+        #                       damage_amt,
+        #                       "Fire",
+        #                       msg)], "")
+    
+    def dirty_tricks(self) -> Tuple:
+        msg: str = "Bandit Rogue attacks for <value> Physical damage"
+        damage_amt = int((3 * self.attack_power) / 4)
+        return ("Attack", damage_amt, "Physical", msg)
+        # return CombatAction([("Attack",
+        #                       damage_amt,
+        #                       "Physical",
+        #                       msg)], "")
+    
+    # def increased_band_size(self):
+    
+    def get_action(self):
+        actions = [self.fireball(),
+                   self.fireball(),
+                   self.dirty_tricks()]
+        index = randint(0, (len(actions) - 1))
+        option = actions.pop(index)
+        return option
+    
+    def take_turn(self) -> CombatAction:
+        damage: int = self.humanoid_damage(self.modify_damage((self.attack_power)))
+        action = CombatAction([("Attack", damage, "Physical", "Some message"),
+                             ("Attack", damage, "Physical", "Second message"),
+                             self.get_action()], "")
+        return action
