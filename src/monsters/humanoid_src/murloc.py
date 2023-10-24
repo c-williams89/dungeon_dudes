aad87008fc640e430ab5f9c95c160aa3ba0d9a2c
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-10-24 11:20:00 -0400

    murloc class has functionality for take turn with basic attack.

--- src/monsters/humanoid_src/murloc.py
+++ src/monsters/humanoid_src/murloc.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple
 
 from src.dd_data import LimitedDict
 from ..humanoid import Humanoid
+from ...combat_action import CombatAction
 
 class Murloc(Humanoid):
     '''Murloc Module'''
@@ -23,12 +24,42 @@ class Murloc(Humanoid):
                                            "Ice",
                                            "Poison",
                                            "Holy"), default_value=100)
-        
+
     @property
     def damage_modifiers(self):
         return self._dam_modifiers
-    
+
     @property
     def defense_modifiers(self):
         return self._def_modifiers
     
+    @property
+    def tribe_size(self):
+        return self._tribe_size
+
+    def get_skills(self):
+        return {}
+    
+    def get_skills_list(self):
+        skills_list = ["Ice Bolt", "Forage"]
+        return skills_list
+
+    def ice_bolt(self):
+        damage = self.humanoid_damage(self.modify_damage(self.intelligence))
+        msg: str = "Murloc casts Ice Bolt for <value> Ice Damage"
+        return("Attack", damage, "Ice", msg)
+    
+    def get_action(self):
+        actions = []
+        return actions
+
+    def take_turn(self):
+        damage: int = self.humanoid_damage(self.modify_damage(self.attack_power))
+        msg: str = "Murloc attacks, dealing <value> physical damage"
+        attack = ("Attack", damage, "Physical", msg)
+        action_list = []
+        for _ in range(1, self.tribe_size):
+            action_list.append(attack)
+        # action_list.append(self.get_action())
+        action = CombatAction(action_list, "")
+        return action
