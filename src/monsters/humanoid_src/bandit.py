c6fbfd526130b42e584ff3586e7b949fe1247e8f
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-10-23 22:06:27 -0400

    Init created for bandits.

--- src/monsters/humanoid_src/bandit.py
+++ src/monsters/humanoid_src/bandit.py
@@ -1,4 +1,19 @@
 '''Bandit Module for Dungeon Dudes'''
+from typing import Dict, Tuple
 
 class Bandit:
     '''Bandit Module'''
+    stats_structure: Dict[str, Tuple[int]] = {"Hit Points": (75, 20),
+                                              "Strength": (5, 1),
+                                              "Agility": (10, 3),
+                                              "Intelligence": (7, 1),
+                                              "Special": (0, 0)}
+    
+    def __init__(self, level_mod: int):
+        self._hit_points: int = self.stats_structure["Hit Points"][0]
+        self._damage_type = "Physical"
+        super().__init__(f"Pack of Bandits",
+                         level_mod,
+                         self.stats_structure)
+        
+    
