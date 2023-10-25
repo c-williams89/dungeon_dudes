12e4eb55cbc7cc61cbd1cfd80b8d463cbe90e459
michaelyockers <michael.r.yockers@cybbh.space>
2023-10-25 09:33:17 -0400

    Updated: attack().

--- src/monsters/elemental_src/frost_elemental.py
+++ src/monsters/elemental_src/frost_elemental.py
@@ -27,7 +27,7 @@ class FrostElemental(Elemental):
         self._dam_modifiers = LimitedDict(
             ("Ice", (self._damage_type)), default_value=100)
         self._special_count = 0
-        self._max_hit_points = self._hit_points
+        self._brittle_count = 0
         self._options = [self.attack]
 
     def spawn_elemental(self, level_mod: int, elemental_types: list):
@@ -76,7 +76,13 @@ class FrostElemental(Elemental):
         if self._level >= 20:
             self._options.append("improved_frost_splinter")
 
-    def brittle_strikes(self) -> CombatAction:
+    def attack(self) -> CombatAction:
+        '''Attack method for Fire Elemental '''
+        damage: int = self.damage_modify(self._attack_power)
+        message: str = f"{self.name} attacks with for <value> Ice damage"
+        return CombatAction([("Attack", damage, "Ice", message)], "")
+
+    def brittle_strikes(self):
         ''' passive: Frost Elemental Attack reduces their opponents defensive
             modifier to Ice by 5 in addition to the damage dealt.  This effect
             is capped at a reduction of 25.
@@ -101,7 +107,7 @@ class FrostElemental(Elemental):
         pass
 
     def improved_freeze(self) -> CombatAction:
-        ''' passive:  After the defensive Physical  modifier for Freeze reaches
+        ''' passive:  After the defensive Physical modifier for Freeze reaches
             its maximum effect, subsequent casts of Freeze deal twice as much
             damage.
         '''
